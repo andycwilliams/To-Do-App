0bce50c09c6e37dca88c7d1033f3a1139e609946
Andy <awilliamscomposing@gmail.com>
2024-01-15 20:38:27 -0800

    Building functions

--- script.js
+++ script.js
@@ -2,7 +2,68 @@ const todoBtn = document.querySelector(".todoBtn");
 
 let todoList = [];
 
+// Initialize
+
+const init = () => {
+  const storedTodos = JSON.parse(localStorage.getItem("todoStorage"));
+  if (storedTodos !== null) {
+    todoList = storedTodos;
+  }
+};
+
+// Add function to submit button and retrieve input
+
 todoBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("CLICKED");
+
+  const todoValue = document.querySelector("#todoInput").value;
+
+  console.log(todoValue);
+
+  const timestamp = new Date().getTime();
+
+  console.log(timestamp);
+
+  addTodo(`${todoValue}_${timestamp}`, todoValue);
 });
+
+// Add todo to LocalStorage
+
+const addTodo = (key, value) => {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+    console.log(`Todo: "${key}" stored!`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+
+  localStorageLog();
+};
+
+// Log everything currently stored in localStorage
+
+const localStorageLog = () => {
+  console.log("Currently in localStorage...");
+  try {
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      const value = JSON.parse(localStorage.getItem(key));
+      console.log(`Key: ${key} \n Value: `, value);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+// Remove todo from LocalStorage
+
+const removeTodo = (key) => {
+  try {
+    localStorage.removeItem(key);
+    console.log(`Todo "${key}" removed!`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+init();
--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Serve static files (including index.html) from the current directory
 app.use(express.static(__dirname));
 
-// Define a route to handle requests for the root URL
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
