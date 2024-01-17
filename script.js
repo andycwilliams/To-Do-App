41b95bb04074db994ff3d0a2a52007d65cf6330e
Andy <awilliamscomposing@gmail.com>
2024-01-16 18:37:25 -0800

    Adding sample data, appending to DOM

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const todoBtn = document.querySelector(".todoBtn");
+const clearBtn = document.querySelector("#clearBtn");
+const todoBtn = document.querySelector("#todoBtn");
+const todoCount = document.querySelector("#todoCount");
+const todoListEl = document.querySelector("#todoListEl");
+const todoOutput = document.querySelector("#todoOutput");
 
 let todoList = [];
 
@@ -6,9 +10,12 @@ let todoList = [];
 
 const init = () => {
   const storedTodos = JSON.parse(localStorage.getItem("todoStorage"));
+
   if (storedTodos !== null) {
     todoList = storedTodos;
   }
+
+  populateTodos();
 };
 
 // Add function to submit button and retrieve input
@@ -17,11 +24,9 @@ todoBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   const todoValue = document.querySelector("#todoInput").value;
-
   console.log(todoValue);
 
   const timestamp = new Date().getTime();
-
   console.log(timestamp);
 
   addTodo(`${todoValue}_${timestamp}`, todoValue);
@@ -55,6 +60,53 @@ const localStorageLog = () => {
   }
 };
 
+// Get placeholders todos to pre-populate list
+
+const placeholderTodos = async () => {
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/`);
+    const todos = await response.json();
+
+    for (let i = 0; i < 5; i++) {
+      const todo = todos[i];
+      console.log(`Todo ${i + 1}: `, todo);
+
+      let p = document.createElement("p");
+      p.textContent = todo.title;
+      p.setAttribute("data-index", i);
+      p.classList.add("todoLi");
+      todoListEl.appendChild(p);
+    }
+  } catch (error) {
+    console.error("Error retrieving data: ", error);
+  }
+};
+
+// Populate todos in DOM
+
+const populateTodos = () => {
+  placeholderTodos();
+
+  todoList.innerHTML = "";
+  todoCount.textContent = todoList.length;
+
+  // for (let i = 0; i < todoList.length; i++) {
+  //   const todo = todoList[i];
+
+  //   let p = document.createElement("p");
+  //   p.textContent = todo;
+  //   p.setAttribute("data-index", i);
+  //   p.classList.add("todoLi");
+
+  //   const deleteBtn = document.createElement("button");
+  //   deleteBtn.textContent = "X";
+  //   deleteBtn.classList.add("deleteBtn");
+
+  //   p.appendChild(deleteBtn);
+  //   todoListEl.appendChild(p);
+  // }
+};
+
 // Remove todo from LocalStorage
 
 const removeTodo = (key) => {
@@ -62,8 +114,21 @@ const removeTodo = (key) => {
     localStorage.removeItem(key);
     console.log(`Todo "${key}" removed!`);
   } catch (error) {
-    console.error("Error:", error);
+    console.error("Error in removing Todo:", error);
   }
 };
+
+// Remove all todos from localStorage
+
+clearBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  try {
+    localStorage.clear();
+    console.log("All todos removed!");
+  } catch (error) {
+    console.error("Error removing all todos:", error);
+  }
+});
 
 init();
